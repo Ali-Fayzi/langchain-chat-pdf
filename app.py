d8cac975b94506718fc00555b2ade16e3a88b066
Ali-Fayzi <a.fayzi@outlook.com>
2024-01-25 01:02:54 +0330

    create main method with streamlit ui , pdfreader , textsplitter , openai embeddings , vector store , langchain_qa_chain

--- app.py
+++ app.py
@@ -10,3 +10,36 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 
 os.environ['OPENAI_API_KEY'] = ''
+
+
+def main():
+    st.header('Chat with  PDF')
+    pdf = st.file_uploader("Upload your PDF File", type='pdf')
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+        text=""
+        for page in pdf_reader.pages:
+            text+=page.extract_text()
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+        )
+        chunks = text_splitter.split_text(text=text)
+        store_name = pdf.name[:-4]
+        st.write(store_name)
+        
+        embeddings = OpenAIEmbeddings()
+        VectorStore = FAISS.from_texts(chunks, embeddings)
+        st.write('Embeddings Created')
+        query = st.text_input("Ask Question from your PDF File")
+        if query:
+            docs = VectorStore.similarity_search(query=query, k=3)
+            llm = OpenAI()
+            chain = load_qa_chain(llm = llm, chain_type='stuff')
+            with get_openai_callback() as cb:
+                response = chain.run(input_documents=docs, question=query)
+                print(cb)
+            st.write(response)
+if __name__ == '__main__':
+    main()
