ca80318a8ababb8a2db1a67a2e90efe9a7a367fe
Ali-Fayzi <a.fayzi@outlook.com>
2024-01-25 01:01:13 +0330

    import librareis

---
+++ app.py
@@ -0,0 +1,9 @@
+import os
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain_community.llms import OpenAI
+from langchain_community.vectorstores import FAISS
+from langchain_community.callbacks import get_openai_callback
+from langchain_community.embeddings.openai import OpenAIEmbeddings
+from langchain.chains.question_answering import load_qa_chain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
